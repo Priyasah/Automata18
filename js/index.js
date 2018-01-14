0ae9c2f3d93e7ce58e30e0b743753cd62be6da3f
priya <sahpriya97@gmail.com>
2018-01-14 17:30:28 +0530

    second commit

--- js/index.js
+++ js/index.js
@@ -119,8 +119,9 @@ function update() {
 update();
 
 
-
 var button = document.querySelector('.menu-icon');
 button.addEventListener('click', function (){
   button.classList.toggle('open');
+   
+    
 });
